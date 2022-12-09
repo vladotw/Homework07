8f187dff83f6f9509636513287143b4a8ea2c774
Vlad <vladot@ya.ru>
2022-12-09 10:08:23 +0600

    Task1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,98 @@
 public class Main {
     public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+        task7();
+        task8();
+    }
+
+    public static void task1() {
+
+        System.out.println("Задача_1");
+        System.out.println();
+
+        int savedMoney = 15_000;
+        int month = 1;
+
+        while (savedMoney <= 2_460_000 ) {
+            System.out.println("Месяц " + month + " сумма накоплений равна " + savedMoney);
+            savedMoney = savedMoney + 15_000;
+            month = month + 1;
+        }
+
+        System.out.println();
+        System.out.println();
+    }
+
+    public static void task2() {
+
+        System.out.println("Задача_2");
+        System.out.println();
+
+
+        System.out.println();
+        System.out.println();
+    }
+
+    public static void task3() {
+
+        System.out.println("Задача_3");
+        System.out.println();
+
+
+        System.out.println();
+        System.out.println();
+    }
+
+    public static void task4() {
+
+        System.out.println("Задача_4");
+        System.out.println();
+
+
+        System.out.println();
+        System.out.println();
+    }
+
+    public static void task5() {
+
+        System.out.println("Задача_5");
+        System.out.println();
+
+
+        System.out.println();
+        System.out.println();
+    }
+
+    public static void task6() {
+
+        System.out.println("Задача_6");
+        System.out.println();
+
+
+        System.out.println();
+        System.out.println();
+    }
+
+    public static void task7() {
+
+        System.out.println("Задача_7");
+        System.out.println();
+
+
+        System.out.println();
+        System.out.println();
+    }
+    public static void task8() {
+
+        System.out.println("Задача_8");
+        System.out.println();
+
+
 
     }
 }
